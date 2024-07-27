c6cbb085902f230fa11025dc4fc273fd9330d222
Thanh <91793770+ThanhOwO@users.noreply.github.com>
2024-07-27 18:31:45 +0700

    Finalize

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { footerLinks } from '../constants'
 
 const Footer = () => {
   return (
@@ -7,11 +8,11 @@ const Footer = () => {
             <div>
                 <p className='font-semibold text-gray text-xs'>
                     More way to shop: {' '}
-                    <span className='underline text-blue'>
+                    <span className='underline text-blue cursor-pointer'>
                         Find an Apple Store {' '}
                     </span>
                     or {' '}
-                    <span className='underline text-blue'>
+                    <span className='underline text-blue cursor-pointer'>
                         other retailer
                     </span>{' '}
                     near you.
@@ -19,6 +20,23 @@ const Footer = () => {
                 <p className='font-semibold text-gray text-xs'>
                     Or call 00000-040-1966
                 </p>
+            </div>
+
+            <div className='bg-neutral-700 my-5 h-[1px] w-full'/>
+            <div className='flex md:flex-row flex-col md:items-center justify-between'>
+                <p className='font-semibold text-gray text-xs'>
+                    Copyright @ 2024 Apple Inc. All rights reserved.
+                </p>
+                <ul className='flex flex-wrap'>
+                    {footerLinks.map((link, i) => (
+                        <li key={link} className='font-semibold text-gray text-xs cursor-pointer'>
+                            {link}{' '}
+                            {i !== footerLinks.length - 1 && (
+                                <span className='mx-2 cursor-default'> | </span>
+                            )}
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     </footer>
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -54,9 +54,9 @@ const Model = () => {
   return (
     <section className='common-padding'>
         <div className='screen-max-width'>
-            <h1 id='heading' className='section-heading'>
+            <h2 id='heading' className='section-heading'>
                 Take a closer look.
-            </h1>
+            </h2>
 
             <div className='flex flex-col items-center mt-5'>
                 <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
